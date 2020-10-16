311c44cb45ed2086f4ab24ab5fb464555b4a11f2
AsimAlmshni <a.asim-1995@hotmail.com>
2020-10-16 19:09:43 +0300

    'fixing'

--- Bank/ClientApp/src/app/customers-list/customers-list.component.ts
+++ Bank/ClientApp/src/app/customers-list/customers-list.component.ts
@@ -2,15 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 import { CustomerService } from '../core/services/customer.service';
 import { CustomerModel } from '../models/customer.model';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.css']
+  styleUrls: ['./customers-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class CustomersListComponent implements OnInit {
   displayedColumns: string[] = ['CustomerName', 'mainCurrency', 'MainAccountNumber', 'totalBalance'];
-  dataSource: MatTableDataSource<CustomerModel>;// = new MatTableDataSource(ELEMENT_DATA);
+  dataSource: MatTableDataSource<CustomerModel>;
   bankName: string;
 
   constructor(private customerService: CustomerService) { 
