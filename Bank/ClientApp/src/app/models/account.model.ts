4b987b5f0fd140883ddeb51785da24ab7fe1c487
AsimAlmshni <a.asim-1995@hotmail.com>
2020-10-16 19:17:39 +0300

    'fixing'

---
+++ Bank/ClientApp/src/app/models/account.model.ts
@@ -0,0 +1,16 @@
+export class account {
+  AccId: number;
+  AccountNumber: string;
+  Currency: string;
+  Balance: number;
+}
+
+
+
+
+
+        public int AccId { get; set; }
+        public string AccountNumber { get; set; }
+        public string Currency { get; set; }
+        public int CustomerId { get; set; }
+        public double Balance { get; set; }
